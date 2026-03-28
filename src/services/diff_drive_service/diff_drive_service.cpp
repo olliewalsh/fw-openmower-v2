1597283927e85d8b3cb4c908961e7f3262cddc93
Oliver Walsh <owalsh@redhat.com>
2026-03-28 00:25:32 +0000

    Fix diff drive angular velocity units

--- src/services/diff_drive_service/diff_drive_service.cpp
+++ src/services/diff_drive_service/diff_drive_service.cpp
@@ -167,7 +167,8 @@ void DiffDriveService::ProcessStatusUpdate() {
     int32_t d_left = static_cast<int32_t>(left_esc_state_.tacho - last_ticks_left);
     int32_t d_right = static_cast<int32_t>(right_esc_state_.tacho - last_ticks_right);
     float vx = static_cast<float>(d_left - d_right) / (2.0f * dt * static_cast<float>(WheelTicksPerMeter.value));
-    float vr = -static_cast<float>(d_left + d_right) / (2.0f * dt * static_cast<float>(WheelTicksPerMeter.value));
+    float vr = -static_cast<float>(d_left + d_right) /
+               (2.0f * dt * static_cast<float>(WheelTicksPerMeter.value) * static_cast<float>(WheelDistance.value));
     actual_linear_velocity_ = vx;
     actual_angular_velocity_ = vr;
     double data[6]{};
